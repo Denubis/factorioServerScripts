f5abdee1f540fa0cc7051668a4449b1efd04d788
kiska <qingbest@fastmail.fm>
2016-09-26 22:57:32 +1000

    removing sleep, adding delimiter

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
-
-sleep 5
+wget -O gdrive "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
 
 chmod +x gdrive
 
