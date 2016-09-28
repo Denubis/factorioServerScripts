b546f0319186752f34cdb01bb30a1cec381c6a15
kiska <qingbest@fastmail.fm>
2016-09-28 13:35:06 +1000

    added google drive commandline install

--- installDependencies.sh
+++ installDependencies.sh
@@ -11,3 +11,8 @@ sudo apt-get update
 
 #Install common packages
 sudo apt-get -y install byobu p7zip-full fail2ban
+
+#Installing google drive cli
+wget -O /tmp/gdrive "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
+sudo cp /tmp/gdrive /usr/local/bin/gdrive
+sudo chmod a+x /usr/local/bin/gdrive
